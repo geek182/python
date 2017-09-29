b673f2a5335d3b79a628ecc51e02daa3b00b4d2e
Leandro Azevedo <geek182@gmail.com>
2017-09-29 10:22:48 -0300

    used for exercises in python

---
+++ exercises/alphabet.py
@@ -0,0 +1,29 @@
+
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+phrase = (list("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyrq ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"))
+translate = ""
+for letter in phrase:
+    if letter == "y":
+        word = alphabet.index(letter) - 24
+        translate += alphabet[word]
+    elif letter == "z":
+        word = alphabet.index(letter) - 24
+        translate += alphabet[word]
+    elif letter =="(":
+        word = "("
+        translate += word
+    elif letter ==")":
+        word = ")"
+        translate += word
+    elif letter == " ":
+        translate += " "
+    elif letter == ".":
+        translate += "."
+    else:
+        word = alphabet.index(letter) + 2
+        translate += alphabet[word]
+
+print translate
+
+'1,2,3,a,b'.split()
