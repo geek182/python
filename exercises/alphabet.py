3cfc04e5eda4151d708ed77477a95846fdf0faa7
Leandro Azevedo <geek182@gmail.com>
2017-09-29 10:26:35 -0300

    more explanations

--- exercises/alphabet.py
+++ exercises/alphabet.py
@@ -1,4 +1,5 @@
-
+# create to resolve this puzzle
+# http://www.pythonchallenge.com/pc/def/map.html
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 phrase = (list("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyrq ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"))
@@ -25,5 +26,3 @@ for letter in phrase:
         translate += alphabet[word]
 
 print translate
-
-'1,2,3,a,b'.split()
